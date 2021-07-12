8f2f5e5b3da9409075589b95c8306809c795414e
Chaitanya Sharma <chaitanyasharma884@gmail.com>
2021-07-12 18:37:18 +0530

    fonts, scaffolding

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  
-  runApp(
-    Center(
-      child :Text(
-          "Hello Flutter",
-          textDirection: TextDirection.ltr
-      )
+void main() {
+  runApp(new MaterialApp(
+    title: "My Flutter App",
+    home:
+    new Scaffold(
+      appBar: AppBar(title : Text("First Screen"),),
+      body: new Material(
+        color: Colors.blueAccent,
+        child: Center(
+            child: Text(
+              "Hello Flutter",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0),
+            )
+        ),
+      ),
+    )
   ));
-} 
+}
