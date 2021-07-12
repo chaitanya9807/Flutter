36e6e37cd3bb38249e073954a825b01749247011
Chaitanya Sharma <chaitanyasharma884@gmail.com>
2021-07-12 18:59:34 +0530

    refractor, method

---
+++ lib/app_screens/first_screen.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+
+class FirstScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Material(
+      color: Colors.blueAccent,
+      child: Center(
+          child: Text(
+            generateLuckyNumber(),
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0),
+          )
+      ),
+    );
+  }
+
+  String generateLuckyNumber(){
+    var random = Random();
+    int luckyNumber = random.nextInt(10);
+    return "Your lucky number is : $luckyNumber";
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/app_screens/first_screen.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    title: "My Flutter App",
-    home:
-    new Scaffold(
-      appBar: AppBar(title : Text("First Screen"),),
-      body: new Material(
-        color: Colors.blueAccent,
-        child: Center(
-            child: Text(
-              "Hello Flutter",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40.0),
-            )
-        ),
-      ),
-    )
-  ));
+void main() => runApp(new MyFlutterApp());
+
+
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: "My Flutter App",
+        home:
+        new Scaffold(
+          appBar: AppBar(title: Text("First Screen"),),
+          body: FirstScreen()
+        )
+    );
+  }
 }
