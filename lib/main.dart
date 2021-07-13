80d58610a5bb99b789c26f6c8622efd9f4f90e0e
Chaitanya Sharma <chaitanyasharma884@gmail.com>
2021-07-13 19:03:57 +0530

    day2

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_apps/app_screens/first_screen.dart';
+import './app_screens/Home.dart';
 
-void main() => runApp(new MyFlutterApp());
+// void main() => runApp(new MyFlutterApp());
+//
+// class MyFlutterApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return new MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: "My Flutter App",
+//         home: new Scaffold(
+//             appBar: AppBar(
+//               title: Text("First Screen"),
+//             ),
+//             body: FirstScreen()));
+//   }
+// }
 
 
-class MyFlutterApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home:
-        new Scaffold(
-          appBar: AppBar(title: Text("First Screen"),),
-          body: FirstScreen()
-        )
-    );
-  }
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Exploring UI widgets",
+    home: Home(),
+  ));
 }
